83a9da684845252679e4002f0dc25b150697d62c
irao12 <ivanrao@outlook.com>
2021-09-12 12:42:12 -0400

    added one more test

--- main.cpp
+++ main.cpp
@@ -10,23 +10,25 @@ Assignment: Lab 2B
 
 int main(){
 
-  std::cout << "Test#1: L = -5, U = 10" << std::endl;
+  std::cout << "Test#1: L = -5, U = 10\nOutput: " << std::endl;
   print_interval(-5, 10);
   std::cout << std::endl;
 
-  std::cout << "Test#2: L = 6, U = 25" << std::endl;
+  std::cout << "Test#2: L = 6, U = 25\nOutput: " << std::endl;
   print_interval(6, 25);
   std::cout << std::endl;
 
-  std::cout << "Test#3: L = -30, U = -16" << std::endl;
+  std::cout << "Test#3: L = -30, U = -16\nOutput: " << std::endl;
   print_interval(-30, -16);
   std::cout << std::endl;
 
-  std::cout << "Test#4: L = 0, U = 20" << std::endl;
-  print_interval(0, 20);
+  std::cout << "Test#4: L = 0, U = 15\nOutput: " << std::endl;
+  print_interval(0, 15);
   std::cout << std::endl;
 
-
+  std::cout << "Test#5: L = 10, U = 0, excpecting no output\nOutput: ";
+  print_interval(10, 0);
+  std::cout << std::endl;
 
   return 0;
 }
